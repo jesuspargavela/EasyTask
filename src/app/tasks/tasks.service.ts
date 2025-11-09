363a1a6412e731195b28681cd6a8b7b1c7d53a97
jesuspargavela <jesuspargavel@gmail.com>
2025-11-09 15:28:51 -0600

    add local storage to task service

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -8,6 +8,11 @@ import { Assignment } from "./task/task.model";
 export class TaskService {
     private tasks = dummyTasks;
 
+    constructor() {
+        const tasks = localStorage.getItem('tasks');
+        if (tasks) this.tasks = JSON.parse(tasks);
+    }
+
     get taskLength(): number {
         return this.tasks.length;
     }
@@ -18,9 +23,15 @@ export class TaskService {
 
     addTask(task: Assignment): void {
         this.tasks.push(task);
+        this.saveTask();
     }
 
     deleteTask(id: string): void {
         this.tasks = this.tasks.filter(task => task.id !== id);
+        this.saveTask();
+    }
+
+    private saveTask(): void {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
